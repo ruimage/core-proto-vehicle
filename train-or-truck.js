bd21322c39845507f78e869c95bd1de608c0f11b
Anton Valkovsky <ruizave@gmail.com>
2022-05-23 14:29:47 +0300

    удалены общие методы

--- train-or-truck.js
+++ train-or-truck.js
@@ -3,11 +3,6 @@
 function Vehicle(driver) {
   this.driver = driver;
   this.speed = 0;
-}
-function Train(driver) {
-  this.driver = driver;
-  this.type = 'passenger'; // 'passenger', 'freight', 'mail'
-  this.speed = 0;
   this.drive = function (kmh) {
     this.speed = kmh;
     return `${this.driver} driving at ${kmh} kilometers per hour`;
@@ -16,30 +11,25 @@ function Train(driver) {
     this.speed = 0;
     return `${this.driver} has stopped`;
   };
+}
+
+function Train(driver) {
+  this.type = 'passenger'; // 'passenger', 'freight', 'mail'
 }
 
 function Truck(driver) {
-  this.driver = driver;
-  this.speed = 0;
   this.cargo = [];
-  this.drive = function (kmh) {
-    this.speed = kmh;
-    return `${this.driver} driving at ${kmh} kilometers per hour`;
-  };
-  this.stop = function () {
-    this.speed = 0;
-    return `${this.driver} has stopped`;
-  };
   this.loadCargo = function (cargo) {
     return this;
   };
+
+  this.unloadCargo = function () {
+    return this.cargo;
+  };
 }
 
-Truck.prototype.unloadCargo = function () {
-  return this.cargo;
-};
 // экспорт Vehicle, Train, Truc в файл с тестами
-module.exports = { Vehicle, Train, Truck };
+module.exports = {Vehicle, Train, Truck};
 
 // Функции-конструкторы Train и Truck имеют схожие свойства и поведение.
 // Перепишите их так, чтобы избавиться от дублирования одинаковых свойств и методов.
